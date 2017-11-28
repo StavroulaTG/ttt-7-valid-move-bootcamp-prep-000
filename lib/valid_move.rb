567d84900d285be5e9bace96a1e89bea4fae3dd1
Stavroula Guerrero <4stacyg@optonline.net>
2017-11-28 14:33:59 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,12 +1,13 @@
 # code your #valid_move? method here
 def valid_move?(board, index)
 
-  board[index] == "X" || board[index] == "O"
+  if board[index] == "X" || board[index] == "O"
     true
-  board[index] == "X" || board[index] == "O"
-  false
+  elsif
+    board[index] !== "X" || board[index] !== "O"
+    false
 
-end
+  end
 
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
