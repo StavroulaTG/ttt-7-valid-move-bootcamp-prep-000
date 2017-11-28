85d5c7abbaf01211fc00027ba41b43896337d728
Stavroula Guerrero <4stacyg@optonline.net>
2017-11-28 14:34:19 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -4,7 +4,7 @@ def valid_move?(board, index)
   if board[index] == "X" || board[index] == "O"
     true
   elsif
-    board[index] !== "X" || board[index] !== "O"
+    board[index] != "X" || board[index] != "O"
     false
 
   end
