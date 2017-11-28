a5c99b63b24105631394aaf36f80c93f2e0e1a04
Stavroula Guerrero <4stacyg@optonline.net>
2017-11-28 14:03:02 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,11 +1,9 @@
 # code your #valid_move? method here
 def valid_move?(board, index)
   if position_taken?(board, index) == true
-    board[index] = 0
     true
   elsif 
     position_taken?(board, index) == false
-    board[index] = 0
     false
   end
 end
